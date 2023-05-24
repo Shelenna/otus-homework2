bdbd49bd88522a96a3dbf53c49dd825d7abbe679
Shelenna <ea.bunkova@gmail.com>
2023-05-25 01:59:52 +0500

    fix

--- _1/src/main/java/Homework12.java
+++ _1/src/main/java/Homework12.java
@@ -1,31 +1,26 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
 import java.util.Scanner;
 public class Homework12 {
 
-    public static void Main(String[] args) {
-        int[] sortArr = {12, 6, 4, 1, 15, 10};
-        quickSort(sortArr, 0, sortArr.length - 1);
-        for(int i = 0; i < sortArr.length; i++){
-            System.out.print(sortArr[i] + "\n");
-        }
-    }
-
-    public static void quickSort(int[] sortArr, int low, int high) {
+    public static void quickSort(ArrayList<Integer> sortArr, int low, int high) {
         //завершить,если массив пуст или уже нечего делить
-        if (sortArr.length == 0 || low >= high) return;
+        if (sortArr.size() == 0 || low >= high) return;
 
         //выбираем опорный элемент
         int middle = low + (high - low) / 2;
-        int border = sortArr[middle];
+        int border = sortArr.get(middle);
 
         //разделияем на подмассивы и меняем местами
         int i = low, j = high;
         while (i <= j) {
-            while (sortArr[i] < border) i++;
-            while (sortArr[j] > border) j--;
+            while (sortArr.get(i) < border) i++;
+            while (sortArr.get(j) > border) j--;
             if (i <= j) {
-                int swap = sortArr[i];
-                sortArr[i] = sortArr[j];
-                sortArr[j] = swap;
+                int swap = sortArr.get(i);
+                sortArr.set(i, sortArr.get(j));
+                sortArr.set(j, swap);
                 i++;
                 j--;
             }
@@ -36,11 +31,32 @@ public class Homework12 {
         if (high > i) quickSort(sortArr, i, high);
     }
 
-    public static void main(String args[]) {
-        int[] sortArr = {12, 6, 4, 1, 15, 10};
-        quickSort(sortArr, 0, sortArr.length - 1);
-        for(int i = 0; i < sortArr.length; i++){
-            System.out.print(sortArr[i] + "\n");
+    private static ArrayList<Integer> randomNumberList(int arraySize, int arrayRange) {
+
+        ArrayList<Integer> randomList = new ArrayList<>();
+
+        for (int i = 0; i < arraySize; i++) {
+            randomList.add(i, new Random().nextInt(arrayRange) + 1);
         }
+
+        return randomList;
+    }
+
+    public static void main(String args[]) {
+        ArrayList<Integer> arrayList = new ArrayList<>(randomNumberList(10000, 99));
+        ArrayList<Integer> arrayListStandart = new ArrayList<>(randomNumberList(10000, 99));
+
+
+        long startTime = System.nanoTime();
+        quickSort(arrayList, 0, arrayList.size() - 1);
+        long finishTime = System.nanoTime();
+
+
+        long startTimeStandart = System.nanoTime();
+        Collections.sort(arrayListStandart);
+        long finishTimeStandart = System.nanoTime();
+
+        System.out.println((finishTime - startTime) + " - время сортировки созданным алгоритмом");
+        System.out.println((finishTimeStandart - startTimeStandart) + " - время сортировки стандартным алгоритмом");
     }
 }
